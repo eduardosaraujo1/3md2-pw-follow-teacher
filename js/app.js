faae2d181aee4f8bcc7a3040bf8dffd3cd183694
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-03-14 12:44:08 -0300

    feat: speed control and better position compatibility

--- js/app.js
+++ js/app.js
@@ -4,34 +4,48 @@ import { render, player } from "./functions.js";
 console.log("App started");
 
 $(() => {
-    // Button handling
+    // Player size
     $("#player_size_plus").on("click", () => {
-        player.setSize((size) => Math.min(size + 1, 64));
+        player.addSize(0.5);
 
         render();
     });
     $("#player_size_minus").on("click", () => {
-        player.setSize((size) => Math.max(size - 1, 4));
+        player.addSize(-0.5);
 
         render();
     });
     $("#board_reset").on("click", () => {
-        player.setSize(() => 16);
-        player.moveX(0);
-        player.moveY(0);
+        player.setSize(4);
+        player.setX(0);
+        player.setY(0);
+        player.setSpeed(8);
 
         render();
     });
 
+    // Player Speed
+    $("#player_speed_plus").on("click", () => {
+        player.addSpeed(1);
+
+        render();
+    });
+    $("#player_speed_minus").on("click", () => {
+        player.addSpeed(-1);
+
+        render();
+    });
+
+    // Movement
     $(window).on("keydown", (event) => {
         if (event.key === "ArrowRight" || event.key === "d") {
-            player.moveX(1);
+            player.addX(1);
         } else if (event.key === "ArrowLeft" || event.key === "a") {
-            player.moveX(-1);
+            player.addX(-1);
         } else if (event.key === "ArrowUp" || event.key === "w") {
-            player.moveY(-1);
+            player.addY(-1);
         } else if (event.key === "ArrowDown" || event.key === "s") {
-            player.moveY(1);
+            player.addY(1);
         }
 
         render();
--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,12 @@
+const REM_IN_PX = parseFloat(
+    getComputedStyle(document.documentElement).fontSize
+);
+
 export function render() {
     // generic size argument
     $("[data-size]").each((_, e) => {
         let _size;
-        _size = $(e).data("size") * 0.25;
+        _size = $(e).data("size");
         _size += "rem";
 
         $(e).css({ width: _size, height: _size });
@@ -26,58 +30,90 @@ export function render() {
     });
 
     // player size display
-    const playerSize = player.size() * 0.25; // 0.25 converts the tailwind value into REM
+    const playerSize = player.size();
     $("#player_size").html(playerSize);
+
+    // player speed display
+    const playerSpeed = player.speed();
+    $("#player_speed").html(playerSpeed);
 
     console.log("Re-render");
 }
 
 function clampPositionX(pos = 0) {
     let minX = 0;
-    let maxX = $("#game").width() - player.size() * 4; // because the value is the tailwind syntax, which is based on REM * 0.25
+    let maxX = $("#game").width() - player.size() * REM_IN_PX;
 
     return Math.min(Math.max(pos, minX), maxX);
 }
 
 function clampPositionY(pos = 0) {
     let minY = 0;
-    let maxY = $("#game").height() - player.size() * 4;
+    let maxY = $("#game").height() - player.size() * REM_IN_PX;
 
     return Math.min(Math.max(pos, minY), maxY);
 }
 
 export const player = {
-    model: () => $("#player_model"),
+    model: () => {
+        return $("#player_model");
+    },
     size: () => {
-        return player.model().data("size") ?? 16;
+        return player.model().data("size") ?? 4;
     },
-    /** @param {Function} callback */
-    setSize: (callback) => {
-        player.model().data("size", parseInt(callback(player.size())));
+    /** @param {number} size */
+    setSize: (size) => {
+        const MIN_SIZE = 1;
+        const MAX_SIZE = 16;
+
+        size = Math.max(Math.min(parseFloat(size), MAX_SIZE), MIN_SIZE);
+        player.model().data("size", size);
     },
-    speed: 8,
+    /** @param {number} size */
+    addSize: (size) => {
+        size = player.size() + parseFloat(size);
+
+        player.setSize(size);
+    },
     x: () => {
         return player.model().data("x") ?? 0;
+    },
+    /** @param {number} x */
+    setX: (x) => {
+        player.model().data("x", clampPositionX(x));
+    },
+    /** @param {number} x */
+    addX: (x) => {
+        x = x * player.speed() + player.x();
+
+        player.setX(x);
     },
     y: () => {
         return player.model().data("y") ?? 0;
     },
+    /** @param {number} y */
+    setY: (y) => {
+        player.model().data("y", clampPositionY(y));
+    },
     /**
-     * @param {number} x
      * @param {number} y
      */
-    moveX: (x) => {
-        let moveCount;
-        moveCount = x * player.speed + player.x();
-        moveCount = clampPositionX(moveCount);
+    addY: (y) => {
+        y = y * player.speed() + player.y();
 
-        player.model().data("x", moveCount);
+        player.setY(y);
     },
-    moveY: (y) => {
-        let moveCount;
-        moveCount = y * player.speed + player.y();
-        moveCount = clampPositionY(moveCount);
+    speed: () => {
+        return player.model().data("speed") ?? 8;
+    },
+    /** @param {number} speed */
+    setSpeed: (speed) => {
+        player.model().data("speed", speed);
+    },
+    /** @param {number} speed */
+    addSpeed: (speed) => {
+        speed = player.speed() + parseInt(speed);
 
-        player.model().data("y", moveCount);
+        player.setSpeed(speed);
     },
 };
